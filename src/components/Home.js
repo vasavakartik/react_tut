a937abdfe51a6b03d668d6755cbbafa6dad32e63
vasavakartik <vasavakartikkumar@gmail.com>
2022-03-03 09:15:21 +0530

    src and components

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Home = () => {
+
+    var no1= 1
+    var no2 =2
+    var ans =  no1+no2
+    const name =  "Raj"
+  return (
+    <div>
+        <h1>Home</h1>
+        <h2>{ans}</h2>
+        <h3>{name}</h3>
+    </div>
+  )
+}
---
+++ src/components/Stringleng.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Stringleng = () => {
+    const name = "kartik"
+    let length = name.length;
+  return (
+    <div>Stringlength
+
+        <h1>length is :{length}</h1>
+    </div>
+  )
+}
