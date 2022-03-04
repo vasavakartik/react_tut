8f4a921749b67d157be5ee6761d4be33662a4896
vasavakartik <vasavakartikkumar@gmail.com>
2022-03-04 09:08:41 +0530

    form and navabr

--- src/components/SimpleForm.js
+++ src/components/SimpleForm.js
@@ -26,13 +26,13 @@ export const SimpleForm = () => {
         <form onSubmit = {submit}>
             <div class="form-group">
                 <label for="exampleInputEmail1">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" onChange= {(e)=>{emailHandler(e)}}/>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" required onChange= {(e)=>{emailHandler(e)}}/>
                 <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
                 {email}
             </div>
             <div class="form-group">
                 <label for="exampleInputPassword1">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" onChange={(e)=>passwordHandler(e)}/>
+                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" required onChange={(e)=>passwordHandler(e)}/>
             </div>
 
             <button type="submit" class="btn btn-primary">Submit</button>
--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export const Navbar = () => {
   return (
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <Link className="navbar-brand" to="/navbar">Navbar</Link>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
